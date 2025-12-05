b665da80dcd60bb65b8ef80cf2ba7e2e9c0eaaf6
RSL23RSL <lirarenato@yahoo.com.br>
2025-12-05 10:57:20 -0300

    Implementação do terceiro módulo treinar modelo clientes_campanha.csv

--- pipeline/treinar.py
+++ pipeline/treinar.py
@@ -23,13 +23,13 @@ def preparar_dados(df, coluna_target='respondeu_campanha'):
     # TODO 1: Crie X removendo a coluna target e cliente_id do DataFrame
     # Dica: X = df.drop(columns=[coluna_target, 'cliente_id'])
     
-    X = None  # Substitua None pelo código correto
+    X = df.drop(columns=[coluna_target, 'cliente_id'])  # Substitua None pelo código correto
     
     
     # TODO 2: Crie y extraindo apenas a coluna target
     # Dica: y = df[coluna_target]
     
-    y = None  # Substitua None pelo código correto
+    y = df[coluna_target]
     
     
     return X, y
@@ -54,7 +54,9 @@ def dividir_treino_teste(X, y, tamanho_teste=0.2, random_state=42):
     #           X, y, test_size=tamanho_teste, random_state=random_state
     #       )
     
-    X_train, X_test, y_train, y_test = None, None, None, None  # Substitua pelo código
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=tamanho_teste, random_state=random_state
+    )
     
     
     # Mostrar tamanhos
@@ -83,7 +85,7 @@ def treinar_modelo(X_train, y_train):
     # Passo 1: Criar o modelo
     # Dica: modelo = RandomForestClassifier(n_estimators=100, random_state=42)
     
-    modelo = None  # Substitua None pelo código correto
+    modelo = RandomForestClassifier(n_estimators=100, random_state=42)
     
     
     # Passo 2: Treinar o modelo (se foi criado)
@@ -91,7 +93,7 @@ def treinar_modelo(X_train, y_train):
     
     if modelo is not None:
         # TODO 5: Treine o modelo usando .fit()
-        pass  # Substitua pass pelo código correto
+        modelo.fit(X_train, y_train)  # Substitua pass pelo código correto
     
     
     print("✅ Modelo treinado!")
@@ -113,7 +115,7 @@ def salvar_modelo(modelo, caminho='models/modelo_campanha.pkl'):
 # Teste local
 if __name__ == "__main__":
     # Carregar dados
-    df = pd.read_csv("data/clientes_campanha.csv")
+    df = pd.read_csv("../data/clientes_campanha.csv")
     
     # Preparar
     X, y = preparar_dados(df)
